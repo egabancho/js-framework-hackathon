8bc37444e8f60224ba3a28461517a16e16d659ab
Harris Tzovanakis <me@drjova.com>
2018-02-20 10:25:20 +0100

    global: addition of searchapi

---
+++ src/utils/configure.js
@@ -0,0 +1,7 @@
+const config = {
+  url: '/records',
+  method: 'get',
+  baseURL: 'https://zenodo.org/api'
+}
+
+export default config;
---
+++ src/utils/search.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import config from './configure'
+import querystring from 'querystring';
+
+class SearchAPI {
+  constructor(customOptions={}) {
+    this.search = axios.create({...config, ...customOptions});
+    console.log(this.search);
+  }
+
+  get(params) {
+    return this.search.get(querystring.stringify(params));
+  };
+
+  makeRequest(params) {
+
+  }
+};
+
+let search = new SearchAPI();
+
+export default search;
